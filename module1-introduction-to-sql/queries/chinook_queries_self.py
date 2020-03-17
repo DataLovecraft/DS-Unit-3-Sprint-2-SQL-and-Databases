c9b2b0269aeaf1a5b6e3cda3c24cc06df9eb9520
John Mercedes <mercedes.john@gmail.com>
2020-03-17 12:29:07 -0400

    updates to module 1

---
+++ module1-introduction-to-sql/queries/chinook_queries_self.py
@@ -0,0 +1,33 @@
+import os
+import sqlite3
+
+# Path to database
+DB_FILEPATH = os.path.join(os.path.dirname(__file__), "../data/", "chinook.db")
+
+
+# First we create a `connection` object that represents the database
+conn = sqlite3.connect(DB_FILEPATH)
+print('CONNECTION:', conn)
+
+# Then we create a `cursor` object and call its `excecute()` method to
+# perform SQL commands
+c = conn.cursor()
+print('CURSOR', c)
+
+query = """
+SELECT
+    trackid,
+    name,
+    composer,
+    unitprice
+FROM
+    tracks;
+"""
+result = c.execute(query).fetchall()
+print("RESULTS: ", result)
+
+
+
+
+
+
